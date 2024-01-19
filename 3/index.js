d1216add2ce17f05f3bacd4a91811c4ce15e251f
Daniele Bortolotti <29543986+bortolottidev@users.noreply.github.com>
2024-01-19 14:48:06 +0100

    feat(3): add part II

--- 3/index.js
+++ 3/index.js
@@ -14,6 +14,106 @@ const isCharacterNumberOrPointOrUndefined = (character) => {
   return false;
 }
 
+
+const addToTuplesNumbers = (tuples, line, x, y) => {
+  const threeCharactersToCheck = line.slice(x-1, x+2)
+  // 0 0 0 = 0
+  if (!isFinite(threeCharactersToCheck[0]) && !isFinite(threeCharactersToCheck[1]) && !isFinite(threeCharactersToCheck[2])) {
+    return;
+  }
+
+  // 1 0 1 = 2
+  if (isFinite(threeCharactersToCheck[0]) && !isFinite(threeCharactersToCheck[1]) && isFinite(threeCharactersToCheck[2])) {
+    tuples.push([x-1, y]);
+    tuples.push([x+1, y]);
+    return;
+  }
+
+  // 1 0 0 = 1
+  // 1 1 0 = 1
+  // 1 1 1 = 1
+  if (isFinite(threeCharactersToCheck[0])) {
+    tuples.push([x-1, y])
+    return;
+  }
+
+  // 0 0 1 = 1 
+  if (!isFinite(threeCharactersToCheck[1]) && isFinite(threeCharactersToCheck[2])) {
+  tuples.push([x+1, y])
+  return;
+  }
+
+  // 0 1 0 = 1
+  // 0 1 1 = 1
+  tuples.push([x, y])
+}
+
+const getMatchedNumbersRowColumnTuples = (matrix, y, x) => { 
+  const tuples = [];
+  // line up
+  addToTuplesNumbers(tuples, matrix[y-1], x, y-1);
+  // current line
+  if(isFinite(matrix[y][x-1])) {
+    tuples.push([x-1,y]);
+  }
+  // current line
+  if (isFinite(matrix[y][x+1])) {
+    tuples.push([x+1,y]);
+  }
+  // line down
+  addToTuplesNumbers(tuples, matrix[y+1], x, y+1);
+    
+  return tuples;
+}
+
+// find all digits of number recursively
+const getNumberFromXY = (matrix, x, y, direction) => {
+  // quit recursion
+  if (isNaN(matrix[y][x])) {
+    return "";
+  }
+
+  if (direction === "back") {
+    return getNumberFromXY(matrix, x-1, y, "back") + matrix[y][x];
+  }
+  if (direction === "forward") {
+    return matrix[y][x] + getNumberFromXY(matrix, x+1, y, "forward");
+  }
+
+  return getNumberFromXY(matrix, x-1, y, "back") + matrix[y][x] + getNumberFromXY(matrix, x+1, y, "forward");
+}
+
+const calculateEngineValue = (matrix, xyTuples) => {
+  if (xyTuples.length !== 2) { 
+    return 0;
+  }
+
+  let value = 1;
+  for (const [x,y] of xyTuples) {
+    const num = getNumberFromXY(matrix, x, y);
+    value *= Number(num)
+  }
+
+  return value;
+}
+
+const checkLineAndReturnValidSumOfEngineCharacter = (matrixCache, lineNumber) => {
+  let currentLineSum = 0;
+
+  // check line
+  let characterIndex = -1;
+  for (const character of matrixCache[lineNumber]) {
+      characterIndex++;
+
+      if (character === '*') {
+        const tuples = getMatchedNumbersRowColumnTuples(matrixCache, lineNumber, characterIndex);
+        currentLineSum += calculateEngineValue(matrixCache, tuples);
+      }
+  }
+
+  return currentLineSum;
+}
+
 const checkLineAndReturnValidSum = (matrixCache, lineNumber) => {
   let currentLineSum = 0;
   let currentNumber = '';
@@ -66,7 +166,6 @@ const checkLineAndReturnValidSum = (matrixCache, lineNumber) => {
 }
   
 const inputFile = await open('./input.txt');
-// Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games
 let totalPoints = 0;
 
 // 467..114..
@@ -109,3 +208,31 @@ if (lineNumber > 1) {
 console.log(totalPoints);
 
 // PART 2
+const inputFile2 = await open('./input.txt');
+let totalPoints2 = 0;
+{
+  let threeRowsMatrixCache = [];
+  let lineNumber = -1;
+  for await (const line of inputFile2.readLines()) {
+    lineNumber++;
+    // did anyone say slicing window?
+    if (lineNumber > 2) {
+      threeRowsMatrixCache = threeRowsMatrixCache.slice(1);
+    }
+    threeRowsMatrixCache.push(line);
+    // check first line - start
+    if (lineNumber === 1) {
+      totalPoints2 += checkLineAndReturnValidSumOfEngineCharacter(threeRowsMatrixCache, 0);
+    }
+    // check mid line - slicing!
+    if (lineNumber > 1) {
+      totalPoints2 += checkLineAndReturnValidSumOfEngineCharacter(threeRowsMatrixCache, 1);
+    }
+  }
+  // check last line - end
+  if (lineNumber > 1) {
+    totalPoints2 += checkLineAndReturnValidSumOfEngineCharacter(threeRowsMatrixCache, 2);
+  }
+}
+
+console.log(totalPoints2);
